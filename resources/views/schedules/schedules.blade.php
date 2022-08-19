81a5e04a8163c7f5f1faec9c428c5acb43fcab95
Raoul-van-Wijk <94360844+Raoul-van-Wijk@users.noreply.github.com>
2022-08-19 17:30:23 +0200

    Change added route to schedules view

--- resources/views/schedules/schedules.blade.php
+++ resources/views/schedules/schedules.blade.php
@@ -12,7 +12,18 @@
                     All schedules
                 </div>
             </div>
-            @include('schedules.schedule-components.schedules')
+        </div>
+        @if (auth()->user()->hasRole(['teacher', 'admin', 'root']))
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 mt-4">
+                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                    <div class="p-6 bg-white border-b border-gray-200">
+                        <a href="{{ route('registerSchedule') }}">Create a new Schedule</a>
+                    </div>
+                </div>
+            </div>
+        @endif
+
+        @include('schedules.schedule-components.schedules')
         </div>
     </div>
 </x-app-layout>
