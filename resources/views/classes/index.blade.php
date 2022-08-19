a3cb1ffb52660629ef1152f0876a725b419f7bed
Raoul-van-Wijk <94360844+Raoul-van-Wijk@users.noreply.github.com>
2022-08-19 17:31:46 +0200

    Change: added base for class page

--- resources/views/classes/index.blade.php
+++ resources/views/classes/index.blade.php
@@ -4,4 +4,19 @@
             {{ __('Classes') }}
         </h2>
     </x-slot>
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 bg-white border-b border-gray-200">
+                    <a href="{{ route('registerClass') }}">Create new class</a>
+                </div>
+            </div>
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg mt-2">
+                <div class="p-6 bg-white border-b border-gray-200">
+                    <a href="{{ route('registerClass') }}">Add students to class</a>
+                </div>
+            </div>
+            @include('classes.components.class-item')
+        </div>
+    </div>
 </x-app-layout>
