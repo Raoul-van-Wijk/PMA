3606eeeba726ceb361f691624e35257f6b4b6772
Raoul-van-Wijk <94360844+Raoul-van-Wijk@users.noreply.github.com>
2022-08-19 17:23:18 +0200

    Change: Updated SchoolClassController
    Added function that counts the students in a class.
    Added function that stores a class.
    Added functions that loads different views

--- app/Http/Controllers/SchoolClassController.php
+++ app/Http/Controllers/SchoolClassController.php
@@ -4,18 +4,43 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\SchoolClass;
+use App\Models\StudentClass;
 
 class SchoolClassController extends Controller
 {
 
     public function index()
     {
-        return view('classes.index', [
-            'classes' => SchoolClass::all(),
-        ]);
+        $countStudents = self::countStudentInClass();
+        $classes = SchoolClass::all()->groupBy('class_name');
+        return view('classes.index', compact('classes', 'countStudents'));
     }
 
-    public function show($id) {
-        dd($id);
+    public function registerClass()
+    {
+        return view('classes.register');
+    }
+
+    public function storeClass(Request $request)
+    {
+        $request->validate([
+            'class_name' => 'required|unique:school_classes',
+        ]);
+        $class = new SchoolClass();
+        $class->class_name = $request->class_name;
+        $class->save();
+        return redirect()->route('classes')->with('success', 'Class created successfully');
+    }
+
+    public function countStudentInClass()
+    {
+        $res = [];
+        $classnames = SchoolClass::get(['class_id', 'class_name']);
+        foreach ($classnames as $class) {
+            $cn = $class->class_name;
+            $count = StudentClass::where('class_id', '=', $class->class_id)->count();
+            $res[$cn] = $count;
+        }
+        return $res;
     }
 }
