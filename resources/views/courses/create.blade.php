831c1aacee830d052b649ebefa57f8301b0bc971
Raoul-van-Wijk <94360844+Raoul-van-Wijk@users.noreply.github.com>
2022-08-24 15:53:10 +0200

    Change: added inputs to the create course form

--- resources/views/courses/create.blade.php
+++ resources/views/courses/create.blade.php
@@ -14,8 +14,11 @@
             </div>
         </div>
     </div>
+    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+    <x-auth-validation-errors class="mb-4" :errors="$errors" />
+    </div>
     <form method="post" action="{{ route('storeCourse') }}" class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-        {{ csrf_field() }}
+        @csrf
         <div class="flex flex-col">
             <div class="-mt-px">
                 <label for="course_name" class="block text-sm font-medium leading-5 text-gray-700">
@@ -35,10 +38,14 @@
             </div>
             <div class="-mt-px">
                 <label for="year" class="block text-sm font-medium leading-5 text-gray-700">
-                    {{ __('Year') }}
+                    {{ __('Period') }}
                 </label>
                 <div class="mt-1 relative rounded-md shadow-sm">
-                    <input id="year" name="year" class="form-input block w-full sm:text-sm sm:leading-5" />
+                    <input id="education" name="education" class="form-input block w-full sm:text-sm sm:leading-5" placeholder="example: Software Developer or SD"/>
+                    <input id="year" name="year" class="form-input block w-full sm:text-sm sm:leading-5" placeholder="year"/>
+                    <input id="semester" name="semester" class="form-input block w-full sm:text-sm sm:leading-5" placeholder="semester"/>
+                    <input id="period" name="period" class="form-input block w-full sm:text-sm sm:leading-5" placeholder="period"/>
+                    <input id="startYear" name="startYear" class="form-input block w-full sm:text-sm sm:leading-5" placeholder="Starting year"/>
                 </div>
             </div>
             <button type="submit">Submit</button>
