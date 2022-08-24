3e2e3d676352bc9e9648ae155790b2a90ac787e0
Raoul-van-Wijk <94360844+Raoul-van-Wijk@users.noreply.github.com>
2022-08-24 15:46:14 +0200

    CHange: added component for schedule
    this components is used to view all schedules with paginations that paginates on yearweek

--- resources/views/schedules/schedule-components/schedules.blade.php
+++ resources/views/schedules/schedule-components/schedules.blade.php
@@ -1,9 +1,25 @@
 <div id="schedules">
     <div class="schedule-comp">
         @foreach($schedules as $i => $schedule)
-            <div class="schedule-item">
-                {{ $schedule[$i]->courses->course_name . '-' . $schedule[$i]->location. ' - '. $schedule[$i]->startdate. ' - '. $schedule[$i]->enddate }}
-            </div>
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 mt-4">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 bg-white border-b border-gray-200">
+            {{ $days[$i] }}
+            @foreach ($schedule as $schedule)
+                <div class="schedule-item">
+                    {{ $schedule->courses->course_name .'-' . $schedule->location.' - '. $schedule->startdate.' - '. $schedule->enddate }}
+                    <a href="{{ route('showSingleSchedule', $schedule->schedule_id) }}">></a>
+                </div>
+            @endforeach
+        </div>
+    </div>
+</div>
         @endforeach
+        <nav>
+            <ul class="pagination">
+                <li class="page-item"><a class="page-link" href="{{ route('allSchedules',$id -1) }}">Previous Week</a></li>
+                <li class="page-item"><a class="page-link" href="{{ route('allSchedules',$id +1) }}">Next Week</a></li>
+            </ul>
+        </nav>
     </div>
 </div>
