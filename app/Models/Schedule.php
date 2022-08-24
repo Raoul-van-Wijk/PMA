7eebeac6d25bfaaec4a99782192b0a2eaefc8b53
Raoul-van-Wijk <94360844+Raoul-van-Wijk@users.noreply.github.com>
2022-08-24 16:21:32 +0200

    added functions Schedule Model
    added the following functions:
        getCourseIdsByClassID,
        getAssignmentIdsFromClassIds,
        getSingleScheduleWithRelations,
        getAllSchedules,
        getTodaysScheduleByClassID,
        getTodaysScheduleByClassID

--- app/Models/Schedule.php
+++ app/Models/Schedule.php
@@ -6,11 +6,14 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use App\Models\Course;
 use App\Models\SchoolClass;
+use App\Models\StudentClass;
 use App\Models\Assignments;
 
 class Schedule extends Model
 {
     use HasFactory;
+
+    protected $primaryKey = 'schedule_id';
 
     protected $fillable = [
         'schedule_id',
@@ -32,7 +35,7 @@ class Schedule extends Model
     }
     public function teacher()
     {
-        return $this->belongsTo('App\Models\User', 'id', 'teacher_id');
+        return $this->belongsTo('App\Models\User', 'teacher_id', 'id');
     }
     public function courses()
     {
@@ -47,7 +50,7 @@ class Schedule extends Model
 
     public static function getScheduleByClassId($id)
     {
-        return Schedule::where('class_id', '=', $id)->with('courses')->get();
+        return Schedule::where('class_id', '=', $id)->where('date', '>=', date('Y-m-d'))->with('courses')->orderBy('date')->get();
     }
     public static function getScheduleByTeacherId($id)
     {
@@ -55,6 +58,40 @@ class Schedule extends Model
     }
     public static function getScheduleById($id)
     {
-        return Schedule::where('schedule_id', '=', $id)->with('courses')->first()->get();
+        return Schedule::where('schedule_id', $id)->with('courses')->limit(1)->get();
+    }
+
+
+
+    public static function getTodaysScheduleByClassID($id)
+    {
+        return Schedule::whereIn('class_id', $id)->where('date', '=', date('Y-m-d'))->with('courses')->orderBy('startdate')->get();
+    }
+
+
+    public static function getAllSchedules($id, $yearweek = null)
+    {
+        if($id == 'root') {
+            return Schedule::where('yearweek', '=', $yearweek)->with('courses')->get();
+        }
+        return Schedule::whereIn('class_id', $id)->where('yearweek', '=', $yearweek)->orderBy('startdate', 'asc')->get();
+    }
+
+    public static function getSingleScheduleWithRelations($id) {
+        return Schedule::where('schedule_id', $id)->with('courses')->with('assignment')->with('teacher')->with('class')->limit(1)->get();
+    }
+
+
+    public static function getCourseIdsByClassID($id)
+    {
+        return Schedule::whereIn('class_id', $id)->get('course_id');
+    }
+
+
+    public static function getAssignmentIdsFromClassIds()
+    {
+
+        return Schedule::whereIn('class_id', StudentClass::where('student_id', auth()->user()->id)->get('class_id'))->get('assignment_id');
     }
 }
+
