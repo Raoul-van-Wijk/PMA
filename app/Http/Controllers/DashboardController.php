24326b4e6be60b4dbea376294e05a910061a6e5b
Raoul-van-Wijk <94360844+Raoul-van-Wijk@users.noreply.github.com>
2022-08-24 16:39:44 +0200

    added more var to send to view

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -6,15 +6,17 @@ use Illuminate\Http\Request;
 use App\Models\Schedule;
 use App\Models\Course;
 use App\Models\Assignments;
+use App\Models\StudentClass;
 
 class DashboardController extends Controller
 {
     public function dashboard()
     {
-
-        $schedules = Schedule::getScheduleByTeacherId(auth()->user()->id)->with('courses')->get();
-        $courses = Course::all();
-        $assignments = Assignments::getAssignmentsBySchedule(auth()->user()->id);
+        $class_ids = StudentClass::getClassIds();
+        $schedules = Schedule::getTodaysScheduleByClassID($class_ids);
+        $courseIds = Schedule::getCourseIdsByClassID($class_ids);
+        $courses = Course::getCoursesByIds($courseIds);
+        $assignments = Assignments::getTodaysAssignments($schedules);
         return view('dashboard', compact('schedules','courses', 'assignments'));
     }
 
