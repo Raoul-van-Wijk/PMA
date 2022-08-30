8c9a4bee681282eccfa39782e09c323341a47099
Raoul-van-Wijk <94360844+Raoul-van-Wijk@users.noreply.github.com>
2022-08-30 18:01:31 +0200

    Added main view of overview page

--- app/Http/Controllers/OverviewController.php
+++ app/Http/Controllers/OverviewController.php
@@ -9,8 +9,15 @@ class OverviewController extends Controller
 {
     public function overview()
     {
-        
-        return view('overview.overview');
+        switch (true):
+            case auth()->user()->hasRole([ 'root','admin', 'teacher' ]):
+                $progress = Progress::getProgressByTeacher(auth()->user()->id);
+                break;
+            default:
+                $progress = Progress::getProgressByStudent(auth()->user()->id);
+                break;
+        endswitch;
+        return view('overview.overview', compact('progress'));
     }
 
 }
