4c43c5e34826fb773084c94e84fcf1f07f331ff7
Raoul-van-Wijk <94360844+Raoul-van-Wijk@users.noreply.github.com>
2022-08-30 18:04:42 +0200

    Laptop to PC

--- resources/views/overview/overview.blade.php
+++ resources/views/overview/overview.blade.php
@@ -3,5 +3,32 @@
         {{ __('Overview') }}
     </x-slot>
 
-
+    <thead>
+        <tr>
+            <th>{{ __('Week 1') }}</th>
+            <th>{{ __('Week 2') }}</th>
+            <th>{{ __('Week 3') }}</th>
+            <th>{{ __('Week 4') }}</th>
+            <th>{{ __('Week 5') }}</th>
+            <th>{{ __('Week 6') }}</th>
+            <th>{{ __('Week 7') }}</th>
+            <th>{{ __('Week 8') }}</th>
+            <th>{{ __('Week 9') }}</th>
+            <th>{{ __('Week 10') }}</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>{{ $overview->week1 }}</td>
+            <td>{{ $overview->week2 }}</td>
+            <td>{{ $overview->week3 }}</td>
+            <td>{{ $overview->week4 }}</td>
+            <td>{{ $overview->week5 }}</td>
+            <td>{{ $overview->week6 }}</td>
+            <td>{{ $overview->week7 }}</td>
+            <td>{{ $overview->week8 }}</td>
+            <td>{{ $overview->week9 }}</td>
+            <td>{{ $overview->week10 }}</td>
+        </tr>
+    </tbody>
 </x-app-layout>
